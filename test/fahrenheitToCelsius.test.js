be5f4e0c1a688e8eb891babaa7d79fe72962e3d9
[Tahir Qayyum] <[qayyumtahir00@gmail.com]>
2020-09-17 00:22:51 -0400

    Removed comments

--- test/fahrenheitToCelsius.test.js
+++ test/fahrenheitToCelsius.test.js
@@ -1,31 +1,31 @@
-// const fahrenheitToCelsius = require('../src/fahrenheitToCelsius')
-// const assert = require('chai').assert
+const fahrenheitToCelsius = require('../src/fahrenheitToCelsius')
+const assert = require('chai').assert
 
 
-// describe("fahrenheitToCelsius() tests", () => {
+describe("fahrenheitToCelsius() tests", () => {
 
-//   it("Converting 32f = 0c ", () => {
+  it("Converting 32f = 0c ", () => {
 
-//     let actVal = fahrenheitToCelsius(32)
-//     let expVal = 0
+    let actVal = fahrenheitToCelsius(32)
+    let expVal = 0
     
-//     assert.equal(actVal, expVal)
-//   })
+    assert.equal(actVal, expVal)
+  })
 
-//   it("Converting 10f = -12.2222c ", () => {
+  it("Converting 10f = -12.2222c ", () => {
 
-//     let actVal = fahrenheitToCelsius(-12.2222)
-//     let expVal = 0
+    let actVal = fahrenheitToCelsius(-12.2222)
+    let expVal = 0
     
-//     assert.equal(actVal, expVal)
-//   })
+    assert.equal(actVal, expVal)
+  })
 
-//   it("Converting 60f = 15.555555555555557c ", () => {
+  it("Converting 60f = 15.555555555555557c ", () => {
 
-//     let actVal = fahrenheitToCelsius(60)
-//     let expVal = 15.555555555555557
+    let actVal = fahrenheitToCelsius(60)
+    let expVal = 15.555555555555557
     
-//     assert.equal(actVal, expVal)
-//   })
+    assert.equal(actVal, expVal)
+  })
 
-// })
+})
